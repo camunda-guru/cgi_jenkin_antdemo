be69bef4a72ca7aaab1bace081112d580aa06068
Parameswari <parameswaribala@gmail.com>
2017-10-25 23:17:22 +0530

    first commit

---
+++ src/com/cgi/utility/MainApp.java
@@ -0,0 +1,14 @@
+package com.cgi.utility;
+
+import java.util.Date;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(getLocalCurrentDate());
+	}
+	private static Date getLocalCurrentDate() {
+		return new Date();
+	}
+}
